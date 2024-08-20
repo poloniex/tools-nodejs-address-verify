2e1eaf3227f31d0fd894f322c746340d2a8d0174
Pzzzzzack <zack.peng@poloniex.com>
2024-08-20 19:26:31 +0800

    init

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+export const PLACEHOLDER = '│   ';
+export const SUMMARY_COLUMNS = ['coin', 'snapshot_height', 'balance'];
+export const DETAIL_COLUMNS = ['coin', 'address', 'snapshot_height', 'balance', 'message', 'signature'];
+export const PASSED = '\u001B[32m✓\u001B[39m';
+export const FAILED = '\u001B[31m✕\u001B[39m';
+export const DETAIL_KEY_MAPPING = {
+  BTC: 'BTC',
+  ETH: 'ETH',
+  TRX: 'TRX',
+  BETH: 'ETH',
+  'HT-ETH': 'ETH',
+  'ETH-Arbitrum': 'ETH',
+  'ETH-Optimism': 'ETH',
+  'USDT-Avalanche C-Chain': 'ETH',
+  'USDT-TRC20': 'TRX',
+  'USDT-ETH': 'ETH',
+  'USDT-BTTC': 'ETH',
+  'USDT-ALGO': 'ALGO',
+  'ETH-BTTC': 'ETH',
+  'BETH-Heco': 'ETH',
+  'BTC-TRC20': 'TRX',
+  'USDT-SOL': 'SOL',
+  'stETH': 'ETH',
+  'stUSDT-TRC20': 'TRX',
+  'USDT-aEthUSDT': 'ETH',
+  'HTX-TRC20': 'TRX',
+  'USDT-Omni': 'BTC'
+}
